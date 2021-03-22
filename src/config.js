b83ebbf74abafb8de1e5309c7d0465d7e7cda710
Andrew Czeropski <acz0919@yahoo.com>
2021-03-21 20:07:08 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "note-app-user-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://om32h2vdl0.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_bvsDS1hMN",
+    APP_CLIENT_ID: "5m47bkuasmmoqhhbngqettsj81",
+    IDENTITY_POOL_ID: "us-east-2:5048ad7a-8f09-405f-85a9-22062f941c49",
+  },
+};
+
+export default config;
